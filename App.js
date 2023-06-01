1a7205eab4be6e9502e049a80b5bee90d240638f
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-01 18:30:04 +0530

    files

--- App.js
+++ App.js
@@ -4,44 +4,49 @@
 //   View 
 // } from 'react-native';
 // import LandingPage from './components/loginsignup';
-
+// // import MainPage from './pages/homepage/MainPage';
 // // import Payment from './components/payment';
 
 
 // export default function App() {
 //   return (
-//     <View style={styles.container}>
+//     <View 
+//     // style={styles.container}
+//     >
 //       {/* <Text>Open up App.js to start working on your  !</Text>
 //       <StatusBar style="auto" /> */}
 //       {/* <Payment/> */}
+//       {/* <MainPage /> */}
 //       <LandingPage/>
 //     </View>
 //   );
 // }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor: '#fff',
+// //     alignItems: 'center',
+// //     justifyContent: 'center',
+// //   },
+// // });
+
 
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ProductsList  from './pages/homepage/ProductsList.js';
+import ProductsList  from './pages/homepage/ProductsList';
 import { ProductDetails } from './pages/homepage/ProductDetails.js';
 
 import Cart  from './pages/homepage/cart.js';
 // import { CartIcon } from '../Services/Carticons.js';
-
 import CartProvider from './pages/homepage/CartContext.js';
-
 import Carticons from './pages/homepage/Carticons.js';
+
+// import LandingPage from './components/loginsignup.js';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -52,49 +57,34 @@ function App() {
     <CartProvider>
 
       <NavigationContainer>
-
+        
         <Stack.Navigator>
-
           <Stack.Screen name='Products' component={ProductsList}
-
             options={({ navigation }) => ({
-
               title: 'Products',
-
               headerTitleStyle: styles.headerTitle,
-
-              headerRight: () => <CartIcon navigation={navigation} />
-
+              headerRight: () => <Carticons navigation={navigation} />
             })} />
 
           <Stack.Screen name='ProductDetails' component={ProductDetails}
 
             options={({ navigation }) => ({
-
               title: 'Product details',
-
               headerTitleStyle: styles.headerTitle,
-
-              headerRight: () => <CartIcon navigation={navigation} />,
-
+              headerRight: () => <Carticons navigation={navigation} />,
             })} />
 
           <Stack.Screen name='Cart' component={Cart}
 
             options={({ navigation }) => ({
-
               title: 'My cart',
-
               headerTitleStyle: styles.headerTitle,
-
-              headerRight: () => <CartIcon navigation={navigation} />,
+              headerRight: () => <Carticons navigation={navigation} />,
 
             })} />
 
         </Stack.Navigator>
-
       </NavigationContainer>
-
     </CartProvider>
 
   );
--- components/loginsignup.js
+++ components/loginsignup.js
@@ -1,7 +1,165 @@
+// import React, { useState } from 'react';
+// import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+// const LandingPage = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [createAccountMode, setCreateAccountMode] = useState(false);
+
+//   const handleLogin = () => {
+//     if (username.trim() === '' || password.trim() === '') {
+//       Alert.alert('Error', 'Please enter a valid username and password');
+//       return;
+//     }
+
+//     // Handle login logic here
+//     console.log('Logging in with:', username, password);
+//   };
+
+//   const handleSignup = () => {
+//     if (
+//       username.trim() === '' ||
+//       password.trim() === '' ||
+//       firstName.trim() === '' ||
+//       lastName.trim() === '' ||
+//       phoneNumber.trim() === '' ||
+//       email.trim() === ''
+//     ) {
+//       Alert.alert('Error', 'Please fill in all the fields');
+//       return;
+//     }
+
+//     // Handle signup logic here
+//     console.log(
+//       'Creating account with:',
+//       firstName,
+//       lastName,
+//       phoneNumber,
+//       email,
+//       username,
+//       password
+//     );
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.heading}>Welcome to the App</Text>
+//       {!createAccountMode ? (
+//         <>
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Username"
+//             onChangeText={setUsername}
+//             value={username}
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Password"
+//             onChangeText={setPassword}
+//             value={password}
+//             secureTextEntry
+//           />
+//           <Button
+//             title="Login"
+//             onPress={handleLogin}
+//           /><br/><br/>
+//           <Button
+//             title="Create Account"
+//             onPress={() => setCreateAccountMode(true)}
+//           />
+//         </>
+//       ) : (
+//         <>
+//           <TextInput
+//             style={styles.input}
+//             placeholder="First Name"
+//             onChangeText={setFirstName}
+//             value={firstName}
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Last Name"
+//             onChangeText={setLastName}
+//             value={lastName}
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Phone Number"
+//             onChangeText={setPhoneNumber}
+//             value={phoneNumber}
+//             keyboardType="phone-pad"
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Email"
+//             onChangeText={setEmail}
+//             value={email}
+//             keyboardType="email-address"
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Username"
+//             onChangeText={setUsername}
+//             value={username}
+//           />
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Password"
+//             onChangeText={setPassword}
+//             value={password}
+//             secureTextEntry
+//           />
+//           <Button
+//             title="Create Account"
+//             onPress={handleSignup}
+//           /> <br/><br/>
+//           <Button
+//             title="Back to Login"
+//             onPress={() => setCreateAccountMode(false)}
+//           />
+//         </>
+//       )}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     padding: 16,
+//   },
+//   heading: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     marginBottom: 16,
+//     textAlign: 'center',
+//   },
+//   input: {
+//     height: 40,
+//     borderColor: 'gray',
+//     borderWidth: 1,
+//     marginBottom: 16,
+//     paddingHorizontal: 8,
+//   },
+// });
+
+// export default LandingPage;
+
+
+
+
+
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Import useNavigation for redirection
 
 const LandingPage = () => {
+  const navigation = useNavigation(); // Create useNavigation instance
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
@@ -10,76 +168,80 @@ const LandingPage = () => {
   const [email, setEmail] = useState('');
   const [createAccountMode, setCreateAccountMode] = useState(false);
 
-const handleLogin = () => {
-  // Prepare the request body
-  const requestBody = {
-    username: username,
-    password: password,
+  const handleLogin = () => {
+    if (username.trim() === '' || password.trim() === '') {
+      Alert.alert('Error', 'Please enter a valid username and password');
+      return;
+    }
+
+    // Handle login logic here
+    console.log('Logging in with:', username, password);
+
+    // Redirect to the home page upon successful login
+    // Replace 'Home' with the actual screen name of your home page
+    navigation.navigate('Home');
   };
 
-  // Send the POST request to the login endpoint
-  fetch('http://localhost:5000/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestBody),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle the response data
-      console.log(data);
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error(error);
-    });
-};
   const handleSignup = () => {
-    // Prepare the request body
-    const requestBody = {
-      username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
-      phoneNumber: phoneNumber,
-      email: email,
-    };
-  
-    // Send the POST request to the signup endpoint
-    fetch('http://localhost:5000/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        console.log(data);
-        // You can add logic here to handle success or failure of the signup process
-        // For example, displaying a success message or redirecting to a different screen
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error(error);
-        // You can add logic here to handle error cases
-        // For example, displaying an error message to the user
-      });
+    if (
+      username.trim() === '' ||
+      password.trim() === '' ||
+      firstName.trim() === '' ||
+      lastName.trim() === '' ||
+      phoneNumber.trim() === '' ||
+      email.trim() === ''
+    ) {
+      Alert.alert('Error', 'Please fill in all the fields');
+      return;
+    }
+
+    // Handle signup logic here
+    console.log(
+      'Creating account with:',
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      username,
+      password
+    );
+
+    // Redirect to the home page upon successful account creation
+    // Replace 'Home' with the actual screen name of your home page
+    navigation.navigate('Home');
   };
-  
+
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Welcome to the App</Text>
-      {createAccountMode ? (
+      {!createAccountMode ? (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={setUsername}
+            value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={setPassword}
+            value={password}
+            secureTextEntry
+          />
+          <Button title="Login" onPress={handleLogin} />
+          <Button
+            title="Create Account"
+            onPress={() => setCreateAccountMode(true)}
+          />
+        </>
+      ) : (
         <>
           <TextInput
             style={styles.input}
             placeholder="First Name"
             onChangeText={setFirstName}
             value={firstName}
-          
           />
           <TextInput
             style={styles.input}
@@ -101,38 +263,26 @@ const handleLogin = () => {
             value={email}
             keyboardType="email-address"
           />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={setUsername}
+            value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={setPassword}
+            value={password}
+            secureTextEntry
+          />
+          <Button title="Create Account" onPress={handleSignup} />
+          <Button
+            title="Back to Login"
+            onPress={() => setCreateAccountMode(false)}
+          />
         </>
-      ) : null}
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={setUsername}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry
-      />
-
-      <Button 
-      // title={!createAccountMode ? 'Create Account' : 'Back to Login'} 
-      title='Back to Login'
-      onPress={handleLogin} />
-
-      {/* <Button title={createAccountMode ? 'Create Account' : 'Login'} 
-      onPress={createAccountMode ? handleCreateAccount : handleLogin}/> */}
-      <Button
-        title={!createAccountMode ? 'Create Account' : 'Back to Login'}
-        // title='Create Account'
-        onPress={() => {
-          handleSignup();
-          setCreateAccountMode(!createAccountMode);
-        }}
-      />
-
+      )}
     </View>
   );
 };
@@ -151,12 +301,11 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: 'green',
+    borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 16,
     paddingHorizontal: 8,
   },
-
 });
 
 export default LandingPage;
