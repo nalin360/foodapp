2f5e7fdf6b7669174f1db92a84591fe1da449ec2
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-01 09:17:43 +0530

    Empty Commit

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// // import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, 
+//   // Text, 
+//   View 
+// } from 'react-native';
+// import LandingPage from './components/loginsignup';
 
-export default function App() {
+// // import Payment from './components/payment';
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       {/* <Text>Open up App.js to start working on your  !</Text>
+//       <StatusBar style="auto" /> */}
+//       {/* <Payment/> */}
+//       <LandingPage/>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ProductsList  from './pages/homepage/ProductsList.js';
+import { ProductDetails } from './pages/homepage/ProductDetails.js';
+
+import Cart  from './pages/homepage/cart.js';
+// import { CartIcon } from '../Services/Carticons.js';
+
+import CartProvider from './pages/homepage/CartContext.js';
+
+import Carticons from './pages/homepage/Carticons.js';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your  !</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <CartProvider>
+
+      <NavigationContainer>
+
+        <Stack.Navigator>
+
+          <Stack.Screen name='Products' component={ProductsList}
+
+            options={({ navigation }) => ({
+
+              title: 'Products',
+
+              headerTitleStyle: styles.headerTitle,
+
+              headerRight: () => <CartIcon navigation={navigation} />
+
+            })} />
+
+          <Stack.Screen name='ProductDetails' component={ProductDetails}
+
+            options={({ navigation }) => ({
+
+              title: 'Product details',
+
+              headerTitleStyle: styles.headerTitle,
+
+              headerRight: () => <CartIcon navigation={navigation} />,
+
+            })} />
+
+          <Stack.Screen name='Cart' component={Cart}
+
+            options={({ navigation }) => ({
+
+              title: 'My cart',
+
+              headerTitleStyle: styles.headerTitle,
+
+              headerRight: () => <CartIcon navigation={navigation} />,
+
+            })} />
+
+        </Stack.Navigator>
+
+      </NavigationContainer>
+
+    </CartProvider>
+
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  headerTitle: {
+
+    fontSize: 20
+
+  }
+
 });
+
+export default App;
