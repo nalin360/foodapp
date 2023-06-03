346a132849ff7667d2c1533af34e16d947a78062
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-03 17:04:35 +0530

    page navigation ProductList ProductDetails add

--- pages/homepage/MainPage.js
+++ pages/homepage/MainPage.js
@@ -1,70 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ProductsList  from './ProductsList.js';
+import ProductsList from './ProductsList.js';
 import { ProductDetails } from './ProductDetails.js';
 
-import Cart  from './cart.js';
-// import { CartIcon } from '../Services/Carticons.js';
+import Cart from './cart.js';
 import CartProvider from './CartContext.js';
 import Carticons from './Carticons.js';
 
-// import LandingPage from './components/loginsignup.js';
+import PaymentWindow from '../paymentsbox/payment';
+import Payment from '../paymentsbox/card.js';
+import SidePanal from '../sidepanal/SidePanal.js';
+import OrderHistoryScreen from '../orderhistorys/OrderHistoryScreen.js';
 
-
+// OrderHistoryScreen
 const Stack = createNativeStackNavigator();
 
 function MainPage() {
+  const [cartItems, setCartItems] = useState([]);
 
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
   return (
-
-    
-
-      <NavigationContainer independent={true}>
-        <CartProvider>
+    <NavigationContainer independent={true}>
+      <CartProvider>
         <Stack.Navigator>
-          <Stack.Screen name='Products' component={ProductsList}
+          <Stack.Screen
+            name="Products"
+            component={ProductsList}
             options={({ navigation }) => ({
               title: 'Products',
               headerTitleStyle: styles.headerTitle,
-              headerRight: () => <Carticons navigation={navigation} />
-            })} />
-
-          <Stack.Screen name='ProductDetails' component={ProductDetails}
-
+              headerLeft : () => <SidePanal navigation={navigation}/>,
+              headerRight: () => <Carticons navigation={navigation} />,
+            })}
+          />
+          <Stack.Screen
+            name="ProductDetails"
+            component={ProductDetails}
             options={({ navigation }) => ({
               title: 'Product details',
               headerTitleStyle: styles.headerTitle,
+              headerLeft : () => <SidePanal navigation={navigation}/>,
               headerRight: () => <Carticons navigation={navigation} />,
-            })} />
+              // Pass the addToCart function as a parameter
 
-          <Stack.Screen name='Cart' component={Cart}
-
+              addToCart: (item) => {
+                navigation.navigate('Cart', { item });
+              },
+            })}
+          />
+          <Stack.Screen
+            name="Cart"
+            component={Cart}
             options={({ navigation }) => ({
               title: 'My cart',
               headerTitleStyle: styles.headerTitle,
+              headerLeft : () => <SidePanal navigation={navigation}/>,
               headerRight: () => <Carticons navigation={navigation} />,
-
-            })} />
-
-        </Stack.Navigator></CartProvider>
-      </NavigationContainer>
-    
-
+            })}
+          />
+          <Stack.Screen
+            name="PaymentWindow"
+            component={PaymentWindow}
+          // options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Payment"
+            component={Payment}
+          options={{ headerShown: false }}
+          />
+          {/*  */}
+          <Stack.Screen
+            name="MainPage"
+            component={MainPage}
+          options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="OrderHistoryScreen"
+            component={OrderHistoryScreen}
+          options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </CartProvider>
+    </NavigationContainer>
   );
-
 }
 
 const styles = StyleSheet.create({
-
   headerTitle: {
-
-    fontSize: 20
-
-  }
-
+    fontSize: 20,
+  },
 });
 
 export default MainPage;
